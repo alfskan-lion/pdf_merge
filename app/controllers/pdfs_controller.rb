a5a72ac9555aec5c77b4d0c709c00911b689f925
leehyojun <alfskan@gmail.com>
2019-09-28 12:18:24 +0000

    integrated rmagick

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,4 +1,6 @@
 class PdfsController < ApplicationController
+  require 'RMagick'
+  include Magick
   before_action :set_pdfs, only: [:selects, :merge, :download_pdf]
   
   def index
@@ -27,12 +29,44 @@ class PdfsController < ApplicationController
   
   def merge
     index = params[:index].to_i
-    pdf = CombinePDF.new
     unless index == 0 
       @pdfs.insert(0, @pdfs.delete_at(index))
     end
     for i in 0..@pdfs.length-1
-      pdf << CombinePDF.load("public/uploads/pdf/#{params[:id]}/#{@pdfs[i].identifier}") 
+      pdf = Magick::ImageList.new("public/uploads/pdf/#{params[:id]}/#{@pdfs[i].identifier}") do
+        self.quality = 80
+        self.density = "300"
+      end
+      unless i == 0
+        FileUtils.mkdir_p "public/uploads/pdf/#{params[:id]}/pdf_img"
+        pdf.write("public/uploads/pdf/#{params[:id]}/pdf_img/washed_#{i}.jpg")
+        pdf_img_list = Dir.glob("public/uploads/pdf/#{params[:id]}/pdf_img/*.jpg").sort
+        for i in 0..pdf_img_list.length-1
+          pdf_img = Magick::ImageList.new("#{pdf_img_list[i]}")
+          anno = Draw.new
+          anno_text = '[별첨1]'
+          anno.annotate(pdf_img, 0,0,40,40, anno_text) {
+          self.font = 'D2Coding'
+          self.fill = 'black'
+          self.stroke = 'transparent'
+          self.pointsize = 25
+          self.font_weight = NormalWeight
+          self.gravity = NorthEastGravity
+          }
+          pdf_img.write("public/uploads/pdf/#{params[:id]}/washed_#{i+1}.pdf")
+          
+        end
+      else
+        pdf.write("public/uploads/pdf/#{params[:id]}/washed_#{i}.pdf")
+      end
+    end
+    # for i in 0..@pdfs.length-1
+    #   pdf << CombinePDF.load("public/uploads/pdf/#{params[:id]}/#{@pdfs[i].identifier}") 
+    # end
+    # pdf.save "public/uploads/pdf/#{params[:id]}/#{@pdfs.first.file.basename}_combined.pdf"
+    pdf = CombinePDF.new
+    for i in 0..pdf_img_list.length
+      pdf << CombinePDF.load("public/uploads/pdf/#{params[:id]}/washed_#{i}.pdf") 
     end
     pdf.save "public/uploads/pdf/#{params[:id]}/#{@pdfs.first.file.basename}_combined.pdf"
     redirect_to download_path(params[:id], index)
